0caed4673a2a91974ac7c41a9ff89da4ab96f3b3
Arthur A. <arthur@thewandb.com>
2024-01-24 12:12:17 +0000

    overhauling handling of events to actually do a full trial run of a single youtube episode (floatplane unsupported right now)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 import "dotenv/config";
 import { spawn } from "child_process";
 import { commandOptions, createClient } from "redis";
-import { Client } from "datakit";
+import { Client, episodeMarkers } from "datakit";
 import ffmpeg from "fluent-ffmpeg";
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync, readFileSync, statSync } from "fs";
 import ytdl from "ytdl-core";
+import {
+	type PutObjectCommandOutput,
+	PutObjectCommand,
+	S3Client,
+} from "@aws-sdk/client-s3";
+import { eq } from "drizzle-orm";
+
+const s3 = new S3Client([
+	{
+		region: "auto",
+		endpoint: `https://${process.env.CF_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+		credentials: {
+			accessKeyId: process.env.CF_ACCESS_KEY_ID,
+			secretAccessKey: process.env.CF_SECRET_ACCESS_KEY,
+		},
+	},
+]);
 
 const redis = createClient({
 	username: process.env.REDIS_USER,
@@ -12,27 +29,44 @@ const redis = createClient({
 	url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
 });
 
+const db = new Client();
+
 interface TranscriptionTask {
 	id: string;
 	kind: string;
 	vod: string;
 }
 
+interface RedisTask {
+	id: string;
+	message: {
+		kind: "youtube" | "floatplane";
+		id: string;
+		vod: string;
+	};
+}
+
 redis
 	.connect()
 	.then(async () => {
+		await redis.xGroupCreate("vods", "whisper", "0", {
+			MKSTREAM: true,
+		});
+
 		while (true) {
 			try {
-				const [{ messages: tasks }]: any = await redis.xRead(
+				const [{ messages: tasks }]: any = await redis.xReadGroup(
 					commandOptions({
 						isolated: true,
 					}),
+					"whisper",
+					"scribe",
 					[
 						// XREAD can read from multiple streams, starting at a
 						// different ID for each...
 						{
 							key: "vods",
-							id: "0",
+							id: ">",
 						},
 					],
 					{
@@ -42,24 +76,64 @@ redis
 					},
 				);
 
-				const { message }: { message: TranscriptionTask } = tasks[0];
-				const { id: episode, kind, vod } = message;
+				console.log(tasks[0]);
+
+				const { id, message } = tasks[0] as RedisTask;
+				const { id: episode, kind, vod } = message as TranscriptionTask;
 
 				console.log(message);
 				switch (kind) {
 					case "youtube":
 						await downloadVideo(vod);
 						await transcribeAudio(vod);
-						process.exit();
+						await upload(
+							`./transcribed/${vod}.txt`,
+							`transcripts/${episode}_yt.txt`,
+						);
+						await upload(
+							`./transcribed/${vod}.json`,
+							`transcripts/${episode}_yt.json`,
+						);
+						await upload(
+							`./transcribed/${vod}.vtt`,
+							`transcripts/${episode}_yt.vtt`,
+						);
+						await upload(
+							`./transcribed/${vod}.srt`,
+							`transcripts/${episode}_yt.srt`,
+						);
+						await db.data
+							.update(episodeMarkers)
+							.set({ youtubeCaptions: true })
+							.where(eq(episodeMarkers.id, episode));
+						await redis.xAck("vods", "whisper", id);
+						break;
 				}
 
-				await redis.xAck("vods", "transcode", tasks[0].id as string);
+				process.exit();
 			} catch (e) {
 				console.error(e);
 			}
 		}
 	})
 	.catch(() => {});
+
+async function upload(
+	file: string,
+	name: string,
+): Promise<PutObjectCommandOutput> {
+	const stat = statSync(file);
+	const body = readFileSync(file);
+
+	return await s3.send(
+		new PutObjectCommand({
+			Key: name,
+			Bucket: process.env.CF_BUCKET_NAME ?? "cdn",
+			Body: body,
+			ContentLength: stat.size ?? 0,
+		}),
+	);
+}
 
 function downloadVideo(id: string): any {
 	return new Promise<void>((resolve, reject) => {
@@ -104,7 +178,7 @@ async function transcribeAudio(id: string): Promise<void> {
 				"--output_dir",
 				"transcribed",
 				"--model",
-				"medium",
+				"medium.en",
 				"--diarize",
 				"--highlight_words",
 				"True",
